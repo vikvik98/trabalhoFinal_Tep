f06427c76ccdb26c672faa6696b683c17c8c0155
vikvik98 <vinicius.c.mascarenhas@hotmail.com>
2019-07-12 04:39:32 -0300

    Cliente

---
+++ cliente/src/app/models/categoria.ts
@@ -0,0 +1,7 @@
+export class Categoria {
+    constructor(
+        public id: number,
+        public nome: string
+                ) {
+    }
+  }
---
+++ cliente/src/app/models/filme.ts
@@ -0,0 +1,7 @@
+export class Filme {
+    constructor(
+        public id: number,
+        public nome: string
+                ) {
+    }
+  }
